f9743910ac1bfc4726171c5319e2cd979feac766
Михайлов Сергей <marrog1@mail.ru>
2016-07-14 12:11:29 +0300

    new update

--- app/src/main/java/com/marrog/messenger/ReceiveMessageActivity.java
+++ app/src/main/java/com/marrog/messenger/ReceiveMessageActivity.java
@@ -2,16 +2,12 @@ package com.marrog.messenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class CreateMessageActivity extends AppCompatActivity {
+public class ReceiveMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_message);
-    }
-
-    public void onSendMessage(View view) {
+        setContentView(R.layout.activity_receive_message);
     }
 }
